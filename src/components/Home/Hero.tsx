7759657f2bca65f322a3d9dfd7caa850e3c0c622
kei0710KEI <keisuke0710.taka@gmail.com>
2025-03-06 21:47:12 +0900

    video

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 
@@ -12,8 +14,9 @@ const Hero = () => {
         autoPlay
         muted
         loop
+        playsInline
         preload="metadata"
-        className="w-full h-full  object-cover"
+        className="w-full h-full object-cover"
       />
       {/* text content */}
       <div className="absolute z-[100] w-full h-full top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
@@ -23,9 +26,12 @@ const Hero = () => {
               Let's Create Your Trip
             </h1>
           </div>
-          <Link href="/create-trip" className="rounded px-14 md:px-28 -mt-4 py-2.5 overflow-hidden group bg-rose-600 relative hover:bg-gradient-to-r hover:from-red-500 hover:to-red-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-red-400 transition-all ease-out duration-300" >
-          <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
-          <span className="relative font-bold text-xl">Create Trip</span>
+          <Link
+            href="/create-trip"
+            className="rounded px-14 md:px-28 -mt-4 py-2.5 overflow-hidden group bg-rose-600 relative hover:bg-gradient-to-r hover:from-red-500 hover:to-red-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-red-400 transition-all ease-out duration-300"
+          >
+            <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
+            <span className="relative font-bold text-xl">Create Trip</span>
           </Link>
         </div>
       </div>
